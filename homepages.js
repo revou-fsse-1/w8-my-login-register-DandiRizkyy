e9add55b04e566cc8ccb8caaf65d2f3b2c5d1896
Dandi Rizky <dandirizkyy94@gmail.com>
2023-03-17 20:32:04 +0700

    fixing bugs on homepages  and adding edit features

--- homepages.js
+++ homepages.js
@@ -1,3 +1,9 @@
+const getLocalStorageData = localStorage.getItem("email");
+if (getLocalStorageData !== null) {
+  const headerMessage = document.querySelector(".user-name");
+  headerMessage.innerHTML = `Halo ${getLocalStorageData}, selamat datang di dashboard.`;
+}
+
 // data dummy
 let data = [
   { name: "Mark", age: 20, email: "markzucciceberg@gmail.com" },
@@ -19,9 +25,9 @@ function showUserData() {
     row +=
       "<td><button class='edit' type='button' onclick='editUserData(" +
       i +
-      ")'>Edit</button></td>";
+      ")'>Edit</button>";
     row +=
-      "<td><button class='delete' type='button' onclick='deleteUserData(" +
+      "<button class='delete' type='button' onclick='deleteUserData(" +
       i +
       ")'>Delete</button></td>";
     row += "</tr>";
@@ -31,15 +37,24 @@ function showUserData() {
 
 //menambahkan data
 function addUserData() {
-  let name = document.querySelector("#userInputName");
-  let age = document.querySelector("#userInputAge");
-  let email = document.querySelector("#userInputEmail");
+  let name = document.querySelector("#userInputName").value;
+  let age = document.querySelector("#userInputUmur").value;
+  let email = document.querySelector("#userInputEmail").value;
   data.push({ name: name, age: age, email: email });
   showUserData();
 }
 
 //menghapus data
-function deleteUserData() {
-  data.splice(0, 1);
+function deleteUserData(userData) {
+  data.splice(userData, 1);
+  showUserData();
+}
+
+//edit data
+function editUserData(userData) {
+  let name = prompt("Input Data Nama Baru", data[userData].name);
+  let age = prompt("Input Data Umur Baru", data[userData].age);
+  let email = prompt("Input Data Email Baru", data[userData].email);
+  data[userData] = { name: name, age: age, email: email };
   showUserData();
 }
