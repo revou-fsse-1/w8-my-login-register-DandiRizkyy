b746b583cca375f8918163341d52aa4876aba20f
Dandi Rizky <dandirizkyy94@gmail.com>
2023-03-17 21:43:56 +0700

    fixing some bugs on regristration and login pages

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ function registerPage() {
   const myRegex = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z.]{2,5}$/;
 
   if (!userEmail) {
-    alert("Isi bro");
+    alert("Isi Emailnya Dulu Donk 😾");
   } else if (!myRegex.test(userEmail)) {
-    alert("Input email yang tepat");
+    alert("Input Email Yang Tepat");
   } else if (!userPassword) {
     alert("Isi Passwordnya Dulu Yok");
   } else if (!validateUpperCase.test(userPassword)) {
     alert(
       "Minimal 1 Uppercase di Paling Depan dan Tidak Boleh Lebih Dari 12 Karakter"
     );
+  } else if (!userPassword.match(validateNumber)) {
+    alert("Minimal 1 Angka Didalam Password");
+  } else if (!userPassword.match(validateLowerCase)) {
+    alert("Minimal 1 Lowercase Didalam Password");
   } else {
-    alert("selamat!");
+    alert("Meow... Registrasi Berhasil !🙀");
     localStorage.setItem("email", userEmail);
     localStorage.setItem("password", userPassword);
     window.location.href = "index.html";
@@ -41,17 +45,23 @@ function loginPage() {
   const myRegex = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z.]{2,5}$/;
 
   if (!userEmail) {
-    alert("Isi bro");
+    alert("Isi emailnya dulu donk 😾");
   } else if (!myRegex.test(userEmail)) {
-    alert("Input email yang tepat");
+    alert("Input Email Yang Tepat");
   } else if (!userPassword) {
     alert("Isi Passwordnya Dulu Yok");
   } else if (!validateUpperCase.test(userPassword)) {
     alert(
       "Minimal 1 Uppercase di Paling Depan dan Tidak Boleh Lebih Dari 12 Karakter"
     );
+  } else if (!validateUpperCase.test(userPassword)) {
+    alert("Minimal 1 Uppercase di Paling Depan");
+  } else if (!userPassword.match(validateNumber)) {
+    alert("Minimal 1 Angka Didalam Password");
+  } else if (!userPassword.match(validateLowerCase)) {
+    alert("Minimal 1 Lowercase Didalam Password");
   } else {
-    alert("Meow! 😸");
+    alert("Meow ! Berhasil Login !😸");
     localStorage.setItem("email", userEmail);
     localStorage.setItem("password", userPassword);
     window.location.href = "homepages.html";
